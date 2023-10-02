4b7ec1dca89e72c85652b0eb931c565743e2ae17
Souhani <souhanikarim1999@gmail.com>
2023-10-02 11:57:37 +0200

    deployment error

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '@/styles/globals.css';
-import '@/styles/HomeStats.css'
+import '/styles/homeStats.css'
 import { SessionProvider } from "next-auth/react"
 
 export default function App({ Component, pageProps: { session, ...pageProps }}) {
