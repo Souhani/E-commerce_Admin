a7c4dc964d5c85a43efe816f4ab243ed1584ca6d
Souhani <souhanikarim1999@gmail.com>
2024-03-14 15:21:32 +0100

    doc

--- components/Layout.js
+++ components/Layout.js
@@ -32,15 +32,6 @@ export default function Layout({ children }) {
       </div>
     )
   }
- if(document) {
-  var cookies = document.cookie.split(";");
-  for (var i = 0; i < cookies.length; i++) {
-      var cookie = cookies[i];
-      var eqPos = cookie.indexOf("=");
-      var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-      document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-  }
- }
    signOut();
   if(status === "unauthenticated"){
     return (
